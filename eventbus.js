0bd0190755391df5c6379fb34caa0ad1a5eb2533
StuPig <shouqiang.gong@gmail.com>
2013-12-05 16:18:36 +0800

    add jshint use strict

--- eventbus.js
+++ eventbus.js
@@ -1,3 +1,6 @@
+/*jshint curly:true, eqeqeq:true, forin:true, noarg:true, noempty:true, nonew:true, undef:true, strict:true, boss:true, es5:true, laxbreak:true, browser:true, devel:true, jquery:true, node:true */
+'use strict';
+
 /**
  * 'all' 任何事件都会触发 args：eventname, arguments
  * 'event' 事件 args：arguments
@@ -5,78 +8,81 @@
  */
 
 var EventBus = {
-        bind: function (names, callback, context) {
-            var events = names.split(/\s+/),
-                calls = this._callbacks || ( this._callbacks = {} );
+    bind: function (names, callback, context) {
+        var events = names.split(/\s+/),
+            calls = this._callbacks || ( this._callbacks = {} );
 
-            for (var i = 0, l = events.length; i < l; i ++) {
-                var ev = events[i],
-                    list = calls[ev] || (calls[ev] = []);
+        for (var i = 0, l = events.length; i < l; i ++) {
+            var ev = events[i],
+                list = calls[ev] || (calls[ev] = []);
 
-                list.push([callback, context]);
-            }
+            list.push([callback, context]);
+        }
 
-            return this;
-        },
-        unbind: function (names, callback) {
-            var events, calls;
+        return this;
+    },
+    unbind: function (names, callback) {
+        var events, calls;
 
-            if (!names || !(events = names.split(/\s+/))) {
-                this._callbacks = {};
-            } else if (calls = this._callbacks){
-                for (var i = 0, len = events.length, ev; i < len && (ev = events[i]); i ++) {
-                    if (!callback) {
-                        calls[ev] = [];
-                    } else {
-                        var list = calls[ev];
-                        if (!list)
-                            return this;
-                        for (var j = 0, l = list.length; j < l; j ++) {
-                            if ( list[j] && callback === list[j][0] ) {
-                                list[j] = null;
-                                break;
-                            }
+        if (!names || !(events = names.split(/\s+/))) {
+            this._callbacks = {};
+        } else if (calls = this._callbacks){
+            for (var i = 0, len = events.length, ev; i < len && (ev = events[i]); i ++) {
+                if (!callback) {
+                    calls[ev] = [];
+                } else {
+                    var list = calls[ev];
+                    if (!list) {
+                        return this;
+                    }
+
+                    for (var j = 0, l = list.length; j < l; j ++) {
+                        if ( list[j] && callback === list[j][0] ) {
+                            list[j] = null;
+                            break;
                         }
                     }
                 }
             }
+        }
 
-            return this;
-        },
-        one: function (names, callback, context) {
-            var _cb = function () {
-                callback.apply(context, arguments);
-                this.unbind(names, _cb);
-            };
-            return this.bind(names, _cb, context);
-        },
-        trigger: function (names) {
-            var events = names.split(/\s+/),
-                both, list, calls, ev, callback, args;
+        return this;
+    },
+    one: function (names, callback, context) {
+        var _cb = function () {
+            callback.apply(context, arguments);
+            this.unbind(names, _cb);
+        };
+        return this.bind(names, _cb, context);
+    },
+    trigger: function (names) {
+        var events = names.split(/\s+/),
+            both, list, calls, ev, callback, args;
 
-            if ( !(calls = this._callbacks) )
-                return this;
-
-            for (var i = 0, len = events.length; i < len && (both = 2); i ++) {
-
-                while ( both -- ) {
-                    ev = both ? 'all' : events[i];
-
-                    if (list = calls[ev]) {
-                        for (var j = 0, l = list.length; j < l; j ++) {
-                            if ( !(callback = list[j]) ) {
-                                list.splice(j, 1);
-                                j --;
-                                l --;
-                            } else {
-                                args = both ? arguments : Array.prototype.slice.call(arguments, 1);
-                                callback[0].apply( callback[1] || this, args );
-                            }
-                        }
-                    }
-                }
-            }
-
+        if ( !(calls = this._callbacks) ) {
             return this;
         }
-    };
+
+        for (var i = 0, len = events.length; i < len && (both = 2); i ++) {
+
+            while ( both -- ) {
+                ev = both ? 'all' : events[i];
+
+                if (list = calls[ev]) {
+                    for (var j = 0, l = list.length; j < l; j ++) {
+                        if ( !(callback = list[j]) ) {
+                            list.splice(j, 1);
+                            j --;
+                            l --;
+                        } else {
+                            args = both ? arguments : Array.prototype.slice.call(arguments, 1);
+                            callback[0].apply( callback[1] || this, args );
+                        }
+                    }
+                }
+            }
+        }
+
+        return this;
+    }
+};
